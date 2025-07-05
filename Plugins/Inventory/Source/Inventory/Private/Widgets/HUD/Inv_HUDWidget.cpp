23674a2bae742a56cfe0c6288096a6753c249504
BhavinMachhi1999 <41407881+BhavinMachhi1999@users.noreply.github.com>
2025-07-05 15:55:05 -0400

    No Room in Inventory

--- Plugins/Inventory/Source/Inventory/Private/Widgets/HUD/Inv_HUDWidget.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/HUD/Inv_HUDWidget.cpp
@@ -2,3 +2,25 @@
 
 
 #include "Widgets/HUD/Inv_HUDWidget.h"
+
+#include "Inv_InfoMessage.h"
+#include "InventoryManagement/Components/Inv_InventoryComponent.h"
+#include "InventoryManagement/Utils/UInv_InventoryStatics.h"
+
+void UInv_HUDWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	UInv_InventoryComponent* InventoryComponent = UUInv_InventoryStatics::GetInventoryComponent(GetOwningPlayer());
+	if (IsValid(InventoryComponent))
+	{
+		InventoryComponent->NoRoomInInventory.AddDynamic(this, &ThisClass::UInv_HUDWidget::OnNoRoom);
+	}
+}
+
+void UInv_HUDWidget::OnNoRoom()
+{
+	if (!IsValid(InfoMessage)) return;
+	InfoMessage->SetMessage(FText::FromString("No Room In Inventory."));		
+	
+}
